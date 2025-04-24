55d17342f6379310718eef2c389265f25b654645
raventid <juliankul@gmail.com>
2025-04-24 17:42:30 +0800

    add primitive Pratt algorithm for left assoc infix

--- bin/main.ml
+++ bin/main.ml
@@ -2,6 +2,70 @@ type binop = Add | Sub | Mul | Div | Pow
 
 type token = TNum of int | TOp of binop
 
+let precedence = function
+| Add | Sub -> 1
+| Mul | Div -> 2
+| Pow -> 3
+
+type expr = Num of int | Op of expr * binop * expr
+
+let prefix (tokens : token list) : (expr * token list) option = match tokens with
+  [] -> None
+| TNum n :: rest -> Some (Num n, rest)
+|  _ -> None
+
+let rec pratt (precedence_limit : int) (tokens : token list) : (expr * token list) option = match prefix tokens with
+  None -> None
+| Some (left_expr, tokens_after_prefix) -> pratt_loop precedence_limit left_expr tokens_after_prefix
+
+and pratt_loop (precendence_limit : int) (expression : expr) (tokens : token list) : (expr * token list) option =
+  (* Next token is an operator, let's figure out its precedence *)
+  match tokens with
+ | TOp operator :: tokens_after_operator -> 
+    let op_precedence = precedence operator in
+    if op_precedence > precendence_limit then
+      (* we can parse it, spawn a child pratt parser *)
+      match ( pratt op_precedence tokens_after_operator ) with
+      | Some (right_expr, tokens_after_child) ->
+        let new_expression = Op (expression, operator, right_expr) in
+          (* -- There might be more on our level *)
+          (* -- (like in 1+2-3), so let's loop. *)
+          pratt_loop precendence_limit new_expression tokens_after_child
+      | None -> 
+        (* -- No more tokens, we are done. *)
+        None
+    else
+      (* We shouldn't parse this op. *)
+      (* Return what we have. *)
+      (* (Note our token list points at the op, *)
+      (* not at the token after.) *)
+      Some (expression, tokens)
+   | _ -> 
+      (* Either we ran out of tokens or found something *)
+      (* that's not an operator. Let's return what we have. *)
+      Some (expression, tokens)
+
+let parse tokens = match (pratt 0 tokens) with
+  Some (expr, _tokens_after_expression) -> Some expr
+| None -> None 
+
+let rec string_of_expr = function
+  | Num n -> string_of_int n
+  | Op (left, op, right) -> 
+      "(" ^ string_of_expr left ^ " " ^ 
+      (match op with
+      | Add -> "+"
+      | Sub -> "-"
+      | Mul -> "*"
+      | Div -> "/"
+      | Pow -> "^") ^ 
+      " " ^ string_of_expr right ^ ")"
+
+let print_expr (parsed_expression: expr option) : unit =
+  match parsed_expression with
+  | None -> print_endline "No valid expression parsed."
+  | Some expr -> print_endline (string_of_expr expr)
+
 let example_tokens = [ TNum 1
     ; TOp Add
     ; TNum 2
@@ -34,4 +98,9 @@ let string_of_token = function
 
 let print_token_stream token_stream = List.iter (fun token -> print_endline (string_of_token token)) token_stream 
 
-let () = print_token_stream example_tokens
+let () = 
+  print_endline "Token stream:";
+  print_token_stream example_tokens;
+
+  print_endline "\nParsed expression:";
+  print_expr (parse example_tokens)
