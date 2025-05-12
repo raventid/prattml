6dd0e937d3f266246fc929b3dea66a55ce6e71e4
raventid <juliankul@gmail.com>
2025-05-12 22:10:32 +0800

    parser: update naming a bit

--- lib/power_pratt.ml
+++ lib/power_pratt.ml
@@ -32,7 +32,7 @@ let parse = string_of_token_stream example_s_expression
 
 (* helper types *)
 exception EndOfTokenStream
-exception ReturnCurrentExpression
+exception LeftBindingPowerIsTooWeak
 exception BadToken
 
 (* The main function to parse an expression using Pratt's algorithm *)
@@ -40,7 +40,7 @@ exception BadToken
 (* The function returns an s_expression representing the parsed expression *)
 (* The precedence limit is used to determine when to stop parsing further expressions *)
 
-let rec expr_bp (token_stream : token Queue.t) (precedence_limit : int) : s_expression =  
+let rec expr_bp (token_stream : token Queue.t) (minimal_binding_power : int) : s_expression =  
   let lhs_ref = ref (match Queue.pop token_stream with
     | T_Atom(it) -> Atom(it)
     | _ -> raise BadToken) in (* bad token *)
@@ -52,8 +52,8 @@ let rec expr_bp (token_stream : token Queue.t) (precedence_limit : int) : s_expr
             | _ -> raise BadToken (* bad token *) in
 
             let (left_bp, right_bp) = infix_binding_power op in
-              if left_bp < precedence_limit then
-                raise ReturnCurrentExpression (* break if precedence is lower than limit *)
+              if left_bp < minimal_binding_power then
+                raise LeftBindingPowerIsTooWeak (* break if precedence is lower than limit *)
               else
                 Queue.pop token_stream |> ignore; (* consume the operator token *)
                 (* check if we need to increase precedence limit *)
@@ -62,7 +62,7 @@ let rec expr_bp (token_stream : token Queue.t) (precedence_limit : int) : s_expr
         done;
         !lhs_ref (* Return the final value *)
       with
-      | ReturnCurrentExpression -> !lhs_ref (* return the current value when we break *)
+      | LeftBindingPowerIsTooWeak -> !lhs_ref (* return the current value when we break *)
       | EndOfTokenStream -> !lhs_ref (* return the current value when we reach the end of the stream *)
       | BadToken -> raise BadToken (* raise an error for bad token, I create token stream manually, so I assume valid programs all the time *)
 
