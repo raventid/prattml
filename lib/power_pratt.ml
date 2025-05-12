5df18978f17219fb841127d35cb6358d5fd7006e
raventid <juliankul@gmail.com>
2025-05-12 21:46:02 +0800

    parser: control parser flow with exceptions

--- lib/power_pratt.ml
+++ lib/power_pratt.ml
@@ -19,49 +19,6 @@ let rec string_of_token_stream = function
       let rest_str = String.concat " " (List.map string_of_token_stream rest) in
       "(" ^ (String.make 1 head) ^ (if rest_str = "" then "" else " " ^ rest_str) ^ ")"
 
-
-let ts = Cons ('a', [Atom 'b'; Cons ('c', [Atom 'd'; Atom 'e'])])
-let parse = string_of_token_stream ts
-
-
-let make_token_stream_example1 : token Queue.t = 
- let q = Queue.create () in
-  Queue.push (T_Atom '1') q;
-  Queue.push (T_Atom '+') q;
-  Queue.push (T_Atom '2') q;
-  Queue.push (T_Atom '*') q;
-  Queue.push (T_Atom '3') q;
-  Queue.push T_Eof q;
-  q
-
-
-(* helper types *)
-exception Break
-
-let rec expr_bp (token_stream : token Queue.t) (precedence_limit : int) : s_expression =  
-  let lhs_ref = ref (match Queue.pop token_stream with
-    | T_Atom(it) -> Atom(it)
-    | _ -> Atom('#')) in (* bad token *)
-      try
-        while true do
-          let op = match Queue.pop token_stream with
-            | T_Eof -> raise Break (* break if end of stream *)
-            | T_Op(it) -> it
-            | _ -> '#' (* bad token *) in
-
-            let (left_bp, right_bp) = infix_binding_power op in
-            if left_bp < precedence_limit then
-              raise Break (* break if precedence is lower than limit *)
-            else
-              let rhs = expr_bp token_stream right_bp in
-              lhs_ref := Cons(op, [!lhs_ref; rhs])
-              (* check if we need to break the loop *)
-        done;
-        !lhs_ref (* Return the final value *)
-      with
-      | Break -> !lhs_ref (* return the current value when we break *)
-
-
 let example_s_expression = 
   Cons ('+', [
     Atom '1'; 
@@ -70,3 +27,42 @@ let example_s_expression =
       Atom '3'
     ])
   ])
+let parse = string_of_token_stream example_s_expression
+
+
+(* helper types *)
+exception EndOfTokenStream
+exception ReturnCurrentExpression
+exception BadToken
+
+(* The main function to parse an expression using Pratt's algorithm *)
+(* It uses a token stream and a precedence limit to build the expression tree *)
+(* The function returns an s_expression representing the parsed expression *)
+(* The precedence limit is used to determine when to stop parsing further expressions *)
+
+let rec expr_bp (token_stream : token Queue.t) (precedence_limit : int) : s_expression =  
+  let lhs_ref = ref (match Queue.pop token_stream with
+    | T_Atom(it) -> Atom(it)
+    | _ -> raise BadToken) in (* bad token *)
+      try
+        while true do
+          let op = match Queue.peek token_stream with
+            | T_Eof -> raise EndOfTokenStream (* break if end of stream *)
+            | T_Op(it) -> it
+            | _ -> raise BadToken (* bad token *) in
+
+            let (left_bp, right_bp) = infix_binding_power op in
+              if left_bp < precedence_limit then
+                raise ReturnCurrentExpression (* break if precedence is lower than limit *)
+              else
+                Queue.pop token_stream |> ignore; (* consume the operator token *)
+                (* check if we need to increase precedence limit *)
+                let rhs = expr_bp token_stream right_bp in
+                  lhs_ref := Cons(op, [!lhs_ref; rhs])
+        done;
+        !lhs_ref (* Return the final value *)
+      with
+      | ReturnCurrentExpression -> !lhs_ref (* return the current value when we break *)
+      | EndOfTokenStream -> !lhs_ref (* return the current value when we reach the end of the stream *)
+      | BadToken -> raise BadToken (* raise an error for bad token, I create token stream manually, so I assume valid programs all the time *)
+
--- test/test_prattml.ml
+++ test/test_prattml.ml
@@ -5,28 +5,43 @@ open Prattml.Power_pratt
 let create_token_queue chars =
   let q = Queue.create () in
   String.iter (fun c -> 
-    if c >= '0' && c <= '9' then 
+    if c >= '0' && c <= '9' || (c >= 'a' && c <= 'z') then 
       Queue.add (T_Atom c) q
     else if c = '+' || c = '-' || c = '*' || c = '/' || c = '^' then
       Queue.add (T_Op c) q
+    else if c != ' ' then (* ignore spaces but handle other chars *)
+      failwith ("Unexpected character: " ^ String.make 1 c)
   ) chars;
   Queue.add T_Eof q;
   q
 
+(* Helper function to parse an expression string *)
+let expr s = 
+  let tokens = create_token_queue s in
+  expr_bp tokens 0
+
 (* Test for expr_bp function with a simple number *)
 let test_expr_bp_simple_number _ =
-  let tokens = create_token_queue "1" in
-  let result = expr_bp tokens 0 in
+let result = expr "1" in
   assert_equal (string_of_token_stream result) "1"
+
+(* Test for arithmetic expression with operator precedence *)
+let test_expr_bp_precedence _ =
+  let result = expr "1 + 2 * 3" in
+  assert_equal (string_of_token_stream result) "(+ 1 (* 2 3))"
+
+(* Test for multi-operator expression with complex precedence *)
+let test_expr_bp_complex _ =
+  let result = expr "a + b * c * d + e" in
+  assert_equal (string_of_token_stream result) "(+ (+ a (* (* b c) d)) e)"
 
 (* Define the test suite *)
 let suite =
   "PrattParserTests" >:::
   [
     "test_simple_number" >:: test_expr_bp_simple_number;
-    (* Commenting out the addition test until expr_bp is fully implemented
-    "test_addition" >:: test_expr_bp_addition;
-    *)
+    "test_precedence" >:: test_expr_bp_precedence;
+    "test_complex" >:: test_expr_bp_complex;
   ]
 
 (* Run the tests *)
